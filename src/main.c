6d9675ff38460ec4a1d2479d39c84d5398f7b3a9
rayenxgamer <rayenxgamer@gmail.com>
2025-08-01 16:30:44 +0100

    initial commit, hello world gtk+ 3!

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <gtk/gtk.h>
 
-static void start (GtkApplication* app, gpointer        user_data) {
+static void activate (GtkApplication* app, gpointer        user_data) {
   GtkWidget *window;
 
   window = gtk_application_window_new (app);
@@ -17,7 +17,7 @@ int main (int  argc, char **argv) {
   int status;
 
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (start), NULL);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
 
